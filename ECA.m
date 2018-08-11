ac72e8552d2a1ab6da287613e0d9bec57a1d2728
sqjin <32399212+sqjin@users.noreply.github.com>
2018-08-10 22:58:21 -0700

    Update ECA.m

--- ECA.m
+++ ECA.m
@@ -8,8 +8,12 @@ Ezscored = zscore(full(E));
 s = svd(Ezscored');
 y = s;
 beta = min(size(E,2)/size(E,1),size(E,1)/size(E,2));
+try
 coef = optimal_SVHT_coef(beta, 0)*median(s);
 y( y < coef ) = 0;
 nPC = nnz(y);
+catch
+    nPC = 2;
+end
 [~,score,~,~,explained] = pca(Ezscored','NumComponents',nPC);
 ydata = score; 
